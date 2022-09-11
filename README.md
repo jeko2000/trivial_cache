2b0563e684e15e55862e9bb2ceceed3a7094928e
Johnny Ruiz <johnny@ruiz-usa.com>
2022-09-11 12:21:19 -0400

    Define initial API

---
+++ README.md
@@ -0,0 +1,3 @@
+trivial_cache
+=====
+A trivial cache implementation in [Erlang](https://www.erlang.org).
---
+++ src/trivial_cache.erl
@@ -0,0 +1,11 @@
+-module(trivial_cache).
+-export([insert/2, lookup/1, delete/1]).
+
+insert(_Key, _Value) ->
+    {error, 'Not implemented'}.
+
+lookup(_Key) ->
+    {error, 'Not implemented'}.
+
+delete(_Key) ->
+    {error, 'Not implemented'}.
